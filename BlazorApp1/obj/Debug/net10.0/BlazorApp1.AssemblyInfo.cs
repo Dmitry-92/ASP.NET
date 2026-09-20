e8aa5ef2f341a549d0c34f66d99f308219bed73d
Dmitry-92 <s1ash020692@gmail.com>
2026-09-20 14:14:15 +0300

    Lesson 3 - Bitcoin DONE

--- BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d38b213cc66c0e038f4b7ced2ba40ff314d649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67af52f438f63204958458c2732ae1532a291258")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
